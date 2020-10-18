1f86d17919b6f1e70cbd125366f109fc526d3878
Himanshu Pandey <u19ee064@eed.svnit.ac.in>
2020-10-18 12:43:41 +0530

    Completed See

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+
+using namespace std;
+
 struct Node{
     int data;
     Node *next;
@@ -7,13 +10,32 @@ class CLL{
     private:
         Node *head;
     public:
-        CLL(){
-            this->head = NULL;
+        CLL(int data){
+            Node* temp = new Node;
+            temp->data = data;
+            temp->next =head;
         }
-
-        insertStart(int);
-        see();
+        Node* createNewNode(int);
+        void insertStart(int);
+        void see();
 };
+
+/*
+I created below function in hope of creating a node from start
+but because of OOP constraint was not able to figure out 
+how to connect with Class to initialize
+I thought working with constructor help
+but still it was giving segmentation fault
+do look into it
+
+*/
+Node* CLL::createNewNode(int num){
+    Node* temp = new Node;
+    temp->data = num;
+    temp->next = head;
+    return temp;
+}
+
 void CLL::insertStart(int num){
     Node *temp = new Node;
     temp->data = num;
@@ -37,10 +59,17 @@ void CLL::see(){
     if(head == NULL){
         cout<<"\nCLL is Already Empty";
     }
+    else{
+        Node* temp = head->next;
+        while(temp!=head){
+            cout<<temp->data;
+            temp = temp->next;
+        }
+    }
 }
 int main()
 {
-    CLL obj;
+    CLL obj(3);
     obj.insertStart(9);
     obj.see();
     obj.insertStart(4);
